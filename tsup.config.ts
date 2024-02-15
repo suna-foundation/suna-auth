99417a5fe36f23c828a927c7770e7db0909e64aa
gratenes <69170374+Gratenes@users.noreply.github.com>
2024-02-14 23:41:44 -0500

    Update package version and expand module exports
    The package version has been incremented as additional source files have been included in the module exports. The exported types have also been supplemented for better compatibility with different import mechanisms. The build configuration has also been tweaked for a non-cleaning approach

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "tsup";
+import {defineConfig} from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/client/index.ts"],
+  entry: [
+    "src/index.ts",
+    "src/client/index.ts"
+  ],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
-  clean: true,
+  clean: false,
 });

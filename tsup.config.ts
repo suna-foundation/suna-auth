741d4bc4760f80552edbbf266faf95ff0876420f
gratenes <69170374+Gratenes@users.noreply.github.com>
2024-02-16 03:28:29 -0500

    attempt to fix package json, failed to fix types

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,14 @@ import {defineConfig} from "tsup";
 export default defineConfig({
   entry: [
     "src/index.ts",
-    "src/client/index.ts"
+    "src/types/index.ts",
+    "src/client/index.ts",
+    "src/client",
+    "src/types"
   ],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: false,
 });
